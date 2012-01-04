304745a6a51452e346fe3a18af07c51dd97a8aac
MS <timetravlr@gmail.com>
2012-01-04 15:14:25 -0700

    fstab module matches hostname patterns

---
+++ modules/fstab/manifests/init.pp
@@ -0,0 +1,27 @@
+# /etc/puppet/manifests/classes/fstab.pp
+class fstab {
+# Pull the right file based on hostname match
+ case $hostname {
+	/node/: { 
+		$source = "puppet:///modules/fstab/fstab.node" 
+	}
+	/int1/: {
+		$source = "puppet:///modules/fstab/fstab.int1"
+	}
+	/int2/: {
+		$source = "puppet:///modules/fstab/fstab.int2"
+	}
+    }
+
+    file { "/etc/fstab":
+        owner  => root, group => root, mode => 644,
+        source => $source,
+    }
+
+#    exec { mountall: command => "mkdir /ops; /bin/mount -a",
+#        subscribe   => File["/etc/fstab"],
+#        refreshonly => true,
+#    }
+
+} # end class fstab
+
